1c6aa00365ecba1a7bb82f9e895662f7cd530368
rotarymars <s.goto2050@gmail.com>
2024-10-27 12:23:26 +0900

    💩 Tab completion for coc, though doesn't work

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -42,6 +42,19 @@ vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-vim.keymap.set('n', 'O', require('ufo').openAllFolds)
-vim.keymap.set('n', 'C', require('ufo').closeAllFolds)
+vim.keymap.set('n', 'zo', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zc', require('ufo').closeAllFolds)
+
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+-- Use Tab for trigger completion with characters ahead and navigate
+-- NOTE: There's always a completion item selected by default, you may want to enable
+-- no select by setting `"suggest.noselect": true` in your configuration file
+-- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
+-- other plugins before putting this into your config
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+keymaps.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keymaps.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 
